83e470735a5a3ec1be763d2cd38f679ffca1213a
Ivan Panarusky <eviltechie@joe.to>
2012-10-13 19:54:39 -0400

    stuff

---
+++ src/to/joe/j2mc/votes/command/CancelVoteCommand.java
@@ -0,0 +1,33 @@
+package to.joe.j2mc.votes.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import to.joe.j2mc.core.command.MasterCommand;
+import to.joe.j2mc.votes.J2MC_Votes;
+
+public class CancelVoteCommand extends MasterCommand {
+    
+J2MC_Votes plugin;
+    
+    public CancelVoteCommand(J2MC_Votes votes) {
+        super(votes);
+        plugin = votes;
+    }
+    
+    @Override
+    public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
+        if (plugin.question == null) {
+            sender.sendMessage(ChatColor.RED + "There is no vote in progress");
+            return;
+        }
+        if (plugin.voteCancelable == false) {
+            sender.sendMessage(ChatColor.RED + "This vote cannot be canceled");
+            return;
+        }
+        plugin.getServer().getScheduler().cancelTask(plugin.voteTallyTask);
+        plugin.getServer().broadcastMessage(ChatColor.DARK_AQUA + "The vote in progress has been canceled");
+    }
+
+}
